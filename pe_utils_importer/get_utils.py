ce9933703273210cc177df4e0e50bd17e9b2e208
Jai Jalah <jai.jalah@krakenflex.com>
2024-07-12 21:02:18 +0100

    chore: first commit

--- pe_utils_importer/get_utils.py
+++ pe_utils_importer/get_utils.py
@@ -1,31 +1,35 @@
 import sys
 
-root_repo = "platform_enablement"
+ROOT_REPO = "platform-enablement"
 
 def get_system_path():
     system_path = sys.path[0]
-    return str(system_path)
+
+    return system_path
+
+def get_file(system_path: str, this_is_static_data_functions: bool = False):
+    # Get path to repo where the file lives.
+    head, sep, tail = system_path.partition(ROOT_REPO)
+
+    if this_is_static_data_functions == True:
+        repo_path = head+sep+'/common/src'
+        print(repo_path)
+    else:
+        repo_path = head+sep
+
+    # Dynamically reference repo in order to point to file.
+    sys.path.insert(0, repo_path)
 
 def get_constants(system_path: str):
-    
-    # Get path to repo where Constants.py lives.
-    head, sep, tail = system_path.partition(root_repo)
-    repo_path = head+sep
 
-    # Dynamically reference repo in order to point to relevant Constants.py file.
-    sys.path.insert(0, repo_path)
+    get_file(system_path)
+
     import Constants
-
     return Constants
 
 def get_static_data_functions(system_path: str):
 
-    # Get path to repo where static_data_functions.py lives.
-    head, sep, tail = system_path.partition(root_repo)
-    repo_path = head+sep+'/common/src'
+    get_file(system_path, True)
 
-    # Dynamically reference repo in order to point to file.
-    sys.path.insert(0, repo_path)
     import static_data_functions
-
     return static_data_functions
